80d89bcb4d08ddb87a89252624d685d3911b44c0
llundell <llundell@ymail.com>
2019-03-24 16:59:21 -0800

    Implement rectangle class

--- pyscript.py
+++ pyscript.py
@@ -40,6 +40,22 @@ class Circle(Shape):
 		# TODO showpage here for testing purposes
 		return "newpath \n0 0 " + str(self._radius) + " 0 360 arc stroke \nshowpage"
 
+class Rectangle(Shape):
+	def __init__(self, width, height):
+		self._width		= width
+		self._height	= height
+
+	def export_postscript(self):
+		#todo refactor
+		return ("newpath 0 0 moveto\n"
+				+ str(self._width) + " 0 lineto\n"
+				+ str(self._width) + " " + str(self._height) + " lineto\n"
+				+ "0 "+ str(self._height)+ " lineto\n"
+				+ "closepath\n"
+				+ "stroke\n"
+				+ "showpage\n")
+
+
 
 
 def export_postscript(shape, filename):
@@ -55,5 +71,7 @@ def export_postscript(shape, filename):
 
 # if name is main, code is executed, otherwise its'being imported as module
 if __name__ == "__main__":
-	circle = Circle(40)
-	export_postscript(circle, "test_circle.ps")
+	# circle = Circle(40)
+	# export_postscript(circle, "test_circle.ps")
+	rectangle=Rectangle(40,80)
+	export_postscript(rectangle, "test_rectangle.ps")
