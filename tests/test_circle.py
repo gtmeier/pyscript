c21608907779546c00b289a34d7eed8139a1a5b2
Jake Herrmann <jtherrmann@alaska.edu>
2019-03-26 18:50:52 -0800

    Only import required shape classes in test files.

--- tests/test_circle.py
+++ tests/test_circle.py
@@ -1,18 +1,18 @@
 import unittest
 
-import pyscript
+from pyscript import Circle
 
 
 class CircleTestCase(unittest.TestCase):
 
     def test_width_0(self):
-        circle = pyscript.Circle(0)
+        circle = Circle(0)
         self.assertEqual(circle.width(), 0)
 
     def test_width_1(self):
-        circle = pyscript.Circle(1)
+        circle = Circle(1)
         self.assertEqual(circle.width(), 2)
 
     def test_width_37(self):
-        circle = pyscript.Circle(37)
+        circle = Circle(37)
         self.assertEqual(circle.width(), 74)
--- tests/test_rectangle.py
+++ tests/test_rectangle.py
@@ -1,18 +1,18 @@
 import unittest
 
-import pyscript
+from pyscript import Rectangle
 
 
 class RectangleTestCase(unittest.TestCase):
 
     def test_width_0(self):
-        rectangle = pyscript.Rectangle(0, 5)
+        rectangle = Rectangle(0, 5)
         self.assertEqual(rectangle.width(), 0)
 
     def test_width_1(self):
-        rectangle = pyscript.Rectangle(1, 5)
+        rectangle = Rectangle(1, 5)
         self.assertEqual(rectangle.width(), 1)
 
     def test_width_37(self):
-        rectangle = pyscript.Rectangle(37, 5)
+        rectangle = Rectangle(37, 5)
         self.assertEqual(rectangle.width(), 37)
