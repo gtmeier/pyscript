dacb3e0482850965f97a10bb8d7c22a2b9ba0a98
Jake Herrmann <jtherrmann@alaska.edu>
2019-03-26 19:10:35 -0800

    Add shape-test.py

--- pyscript/__init__.py
+++ pyscript/__init__.py
@@ -23,14 +23,3 @@ def export_postscript(shape, center=Point(0, 0), filename="shape.ps"):
     # closing
     with open(filename, "w+") as output_file:
         output_file.write(postscript_code)
-
-
-# TODO remove main when done with project
-
-# FIXME
-# if name is main, code is executed, otherwise its'being imported as module
-# if __name__ == "__main__":
-#     shape = Circle(40)
-#     # shape = Rectangle(40, 80)
-#     # export_postscript(shape)
-#     export_postscript(shape, Point(100, 100))
