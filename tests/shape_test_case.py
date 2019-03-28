4a43a156e6faa7b83af3fbb9159cc5e0e383c974
Jake Herrmann <jtherrmann@alaska.edu>
2019-03-28 08:04:31 -0800

    Refactor ShapeTestCase._test_export_postscript

--- tests/shape_test_case.py
+++ tests/shape_test_case.py
@@ -21,26 +21,22 @@ class ShapeTestCase(unittest.TestCase):
         shape.export_postscript(center=center, filename=self._test_file_path)
         self.assertTrue(os.path.isfile(self._test_file_path))
 
-        with open(self._test_file_path, 'r') as test_file:
-            test_code = test_file.read()
+        actual_code = self._get_actual_export_code()
+        expected_code = self._get_expected_export_code()
+        self.assertEqual(actual_code, expected_code)
 
-        expected_output_file_path = os.path.join(
+    def _get_actual_export_code(self):
+        with open(self._test_file_path, 'r') as test_file:
+            return test_file.read()
+
+    def _get_expected_export_code(self):
+        export_file_path = os.path.join(
             self._postscript_code_path,
             self._get_test_case_name(),  # TODO: set in constructor
             self._get_current_test_name() + '.ps'
         )
-        with open(expected_output_file_path, 'r') as expected_output_file:
-            expected_code = expected_output_file.read()
-
-        self.assertEqual(test_code, expected_code)
-
-    def _get_current_test_name(self):
-        test_prefix = 'test_export_postscript_'
-        current_test_full_name = (
-            inspect.currentframe().f_back.f_back.f_code.co_name
-        )
-        assert current_test_full_name.startswith(test_prefix)
-        return current_test_full_name[len(test_prefix):]
+        with open(export_file_path, 'r') as export_file:
+            return export_file.read()
 
     def _get_test_case_name(self):
         test_case_suffix = 'TestCase'
@@ -48,3 +44,11 @@ class ShapeTestCase(unittest.TestCase):
         assert test_case_full_name.endswith(test_case_suffix)
         test_case_name_len = len(test_case_full_name) - len(test_case_suffix)
         return test_case_full_name[:test_case_name_len]
+
+    def _get_current_test_name(self):
+        test_prefix = 'test_export_postscript_'
+        current_test_full_name = (
+            inspect.currentframe().f_back.f_back.f_back.f_code.co_name
+        )
+        assert current_test_full_name.startswith(test_prefix)
+        return current_test_full_name[len(test_prefix):]
