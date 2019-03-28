af44ef0cf0394b37e3a42b40bc4e5b3f86298db2
Jake Herrmann <jtherrmann@alaska.edu>
2019-03-27 20:01:55 -0800

    Add Circle._get_postscript tests.

--- tests/test_circle.py
+++ tests/test_circle.py
@@ -1,6 +1,6 @@
 import unittest
 
-from pyscript import Circle
+from pyscript import Circle, Point
 
 
 class CircleTestCase(unittest.TestCase):
@@ -28,3 +28,21 @@ class CircleTestCase(unittest.TestCase):
     def test_height_37(self):
         circle = Circle(37)
         self.assertEqual(circle.height(), 74)
+
+    def test_get_postscript_80_80_80(self):
+        code = Circle(80)._get_postscript(Point(80, 80))
+        self.assertEqual(
+            code,
+            "newpath\n"
+            "80 80 80 0 360 arc\n"
+            "stroke\n"
+        )
+
+    def test_get_postscript_20_160_40(self):
+        code = Circle(40)._get_postscript(Point(20, 160))
+        self.assertEqual(
+            code,
+            "newpath\n"
+            "20 160 40 0 360 arc\n"
+            "stroke\n"
+        )
