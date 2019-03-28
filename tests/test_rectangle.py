fff82b5c1adb986300d697c66b064af5b5751a71
Jake Herrmann <jtherrmann@alaska.edu>
2019-03-27 20:08:51 -0800

    Add Rectangle._get_postscript test.

--- tests/test_rectangle.py
+++ tests/test_rectangle.py
@@ -1,6 +1,6 @@
 import unittest
 
-from pyscript import Rectangle
+from pyscript import Rectangle, Point
 
 
 class RectangleTestCase(unittest.TestCase):
@@ -28,3 +28,16 @@ class RectangleTestCase(unittest.TestCase):
     def test_height_37(self):
         rectangle = Rectangle(5, 37)
         self.assertEqual(rectangle.height(), 37)
+
+    def test_get_postscript(self):
+        code = Rectangle(40, 80)._get_postscript(Point(100, 100))
+        self.assertEqual(
+            code,
+            "newpath\n"
+            "80.0 60.0 moveto\n"
+            "40 0 rlineto\n"
+            "0 80 rlineto\n"
+            "-40 0 rlineto\n"
+            "closepath\n"
+            "stroke\n"
+        )
