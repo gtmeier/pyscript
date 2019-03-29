2a935f8c67cbc3a2d083749a80234a7d6214d19e
Jake Herrmann <jtherrmann@alaska.edu>
2019-03-28 22:10:37 -0800

    Update Square's data member names in SquareTestCase.

--- tests/test_square.py
+++ tests/test_square.py
@@ -7,19 +7,19 @@ class SquareTestCase(unittest.TestCase):
 
     def test_side_length_0(self):
         square = Square(0)
-        self.assertEqual(square._sideLength, 0)
+        self.assertEqual(square._side_length, 0)
 
     def test_side_length_1(self):
         square = Square(1)
-        self.assertEqual(square._sideLength, 1)
+        self.assertEqual(square._side_length, 1)
 
     def test_side_length_54(self):
         square = Square(54)
-        self.assertEqual(square._sideLength, 54)
+        self.assertEqual(square._side_length, 54)
 
     def test_num_sides_4(self):
         square = Square(1)
-        self.assertEqual(square._numSides, 4)
+        self.assertEqual(square._num_sides, 4)
 
     # FIXME
     # def test_get_postscript(self):
