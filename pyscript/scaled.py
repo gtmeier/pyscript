df731d115d4bdea19ab2425cf3d4dcdda98df201
llundell <llundell@ymail.com>
2019-03-28 19:29:02 -0800

    Fix ScaledShape

--- pyscript/scaled.py
+++ pyscript/scaled.py
@@ -6,24 +6,25 @@
 
 from . import Shape
 
+
 class ScaledShape(Shape):
 
-    def __init__(self, scale_factor_x, scale_factor_y):
+    def __init__(self, shape, scale_factor_x, scale_factor_y):
+        self._shape = shape
         self._scale_factor_x = scale_factor_x
         self._scale_factor_y = scale_factor_y
 
     def _get_postscript(self, center):
-        shape_postscript = self._get_toplevel_postscript(shape, center)
+        shape_postscript = self._shape._get_postscript(center)
 
         return self._join_lines(
-            "newpath",
-            f"{scale_factor_x} ",
-            f"{scale_factor_y} ",
+            f"{self._scale_factor_x} "
+            f"{self._scale_factor_y} ",
             "scale",
             f"{shape_postscript}")
 
     def width(self):
-        return self._width
+        return self._shape.width() * self._scale_factor_x
 
     def height(self):
-        return self._height
+        return self._shape.width() * self._scale_factor_y
