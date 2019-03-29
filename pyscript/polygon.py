4fe4673610b855d3f73b1aa5284b72e8adbbc51f
Jake Herrmann <jtherrmann@alaska.edu>
2019-03-28 22:03:00 -0800

    Rename Polygon variables and methods to use snake_case.

--- pyscript/polygon.py
+++ pyscript/polygon.py
@@ -4,68 +4,69 @@ import math
 
 class Polygon(Shape):
 
-    def __init__(self, numSides, sideLength):
-        self._numSides = numSides
-        self._sideLength = sideLength
-        self.determineHW()
+    def __init__(self, num_sides, side_length):
+        self._num_sides = num_sides
+        self._side_length = side_length
+        self._calculate_height_width()
 
-    def setWidth(self, width):
+    def set_width(self, width):
         self._width = width
 
-    def setHeight(self, height):
+    def set_height(self, height):
         self._height = height
 
-    def getWidth(self):
+    def get_width(self):
         return self._width
 
-    def getHeight(self):
+    def get_height(self):
         return self._height
 
-    def determineHW(self):
-        if self._numSides % 2 != 0:
-            self.setHeight(
-                self._sideLength
-                * (1 + math.cos(math.pi / self._numSides))
-                / (2 * math.sin(math.pi / self._numSides))
+    def _calculate_height_width(self):
+        if self._num_sides % 2 != 0:
+            self.set_height(
+                self._side_length
+                * (1 + math.cos(math.pi / self._num_sides))
+                / (2 * math.sin(math.pi / self._num_sides))
             )
-            self.setWidth(
-                (self._sideLength
+            self.set_width(
+                (self._side_length
                  * math.sin(math.pi
-                            * (self._numSides - 1) / (2 * self._numSides)))
-                / (math.sin(math.pi / self._numSides))
+                            * (self._num_sides - 1) / (2 * self._num_sides)))
+                / (math.sin(math.pi / self._num_sides))
             )
-        elif self._numSides % 4 == 0:
-            self.setHeight(
-                self._sideLength
-                * (math.cos(math.pi / self._numSides))
-                / (math.sin(math.pi / self._numSides))
+        elif self._num_sides % 4 == 0:
+            self.set_height(
+                self._side_length
+                * (math.cos(math.pi / self._num_sides))
+                / (math.sin(math.pi / self._num_sides))
             )
-            self.setWidth(
-                (self._sideLength * math.sin(math.pi / self._numSides))
-                / (math.sin(math.pi / self._numSides))
+            self.set_width(
+                (self._side_length * math.sin(math.pi / self._num_sides))
+                / (math.sin(math.pi / self._num_sides))
             )
         else:
-            self.setHeight(
-                self._sideLength
-                * (math.cos(math.pi / self._numSides))
-                / (math.sin(math.pi / self._numSides))
+            self.set_height(
+                self._side_length
+                * (math.cos(math.pi / self._num_sides))
+                / (math.sin(math.pi / self._num_sides))
             )
-            self.setWidth(self._sideLength/(math.sin(math.pi/self._numSides)))
+            self.set_width(self._side_length
+                           / (math.sin(math.pi / self._num_sides)))
 
     def _get_postscript(self, center):
-        totalAngle = (self._numSides - 2) * 180  # formula for interior angles
-        interiorAngle = str(180 - (totalAngle / self._numSides))
-        sidesMinusOne = str(self._numSides - 1)
+        totalAngle = (self._num_sides - 2) * 180  # formula for interior angles
+        interiorAngle = str(180 - (totalAngle / self._num_sides))
+        sidesMinusOne = str(self._num_sides - 1)
 
-        translateX = str(self._sideLength / -2)
-        translateY = str(self.getHeight() / -2)
+        translateX = str(self._side_length / -2)
+        translateY = str(self.get_height() / -2)
 
         return self._join_lines(
             "gsave ",
             translateX + " " + translateY + " translate newpath ",
             f"{center.x} {center.y} moveto ",
             "1 1 " + sidesMinusOne + " { ",
-            str(self._sideLength) + " 0 rlineto ",
+            str(self._side_length) + " 0 rlineto ",
             interiorAngle + " rotate ",
             "} for ",
             "closepath",
