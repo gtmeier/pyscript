28d7b57fe55ed92b2cfce52903adc44d6d92dff6
llundell <llundell@ymail.com>
2019-03-29 15:44:32 -0800

    Add tests for triangle

---
+++ tests/test_triangle.py
@@ -0,0 +1,21 @@
+import unittest
+
+from pyscript import Triangle
+
+
+class TriangleTestCase(unittest.TestCase):
+    def test_side_length_0(self):
+        triangle = Triangle(0)
+        self.assertEqual(triangle.sideLength(), 0)
+
+    def test_side_length_1(self):
+        triangle = Triangle(1)
+        self.assertEqual(triangle.sideLength(), 1)
+
+    def test_side_length_54(self):
+        triangle = Triangle(54)
+        self.assertEqual(triangle.sideLength(), 54)
+
+    def test_num_sides_3(self):
+        triangle = Triangle(1)
+        self.assertEqual(triangle.numSides(), 3)
