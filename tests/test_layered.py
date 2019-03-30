b5b29c8ae921bbeea4b21b68395cc944dc4af493
Jake Herrmann <jtherrmann@alaska.edu>
2019-03-30 09:45:03 -0800

    Add tests for LayeredShapes._get_width

---
+++ tests/test_layered.py
@@ -0,0 +1,24 @@
+from pyscript import LayeredShapes, Circle, Rectangle
+
+from shape_test_case import ShapeTestCase
+
+
+class LayeredShapesTestCase(ShapeTestCase):
+
+    def test_get_width_no_shapes(self):
+        shape = LayeredShapes()
+        self.assertEqual(shape._get_width(), 0)
+
+    def test_get_width_single_shape(self):
+        shape = LayeredShapes(Circle(3))
+        self.assertEqual(shape._get_width(), 6)
+
+    def test_get_width_multiple_shapes(self):
+        shape = LayeredShapes(
+            Circle(1),
+            Rectangle(5, 10),
+            Circle(21),
+            Rectangle(0, 1),
+            Rectangle(3, 9)
+        )
+        self.assertEqual(shape._get_width(), 42)
