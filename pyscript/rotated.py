2aff5e909d9a7dcf32c638666ec65f596c99c136
Jake Herrmann <jtherrmann@alaska.edu>
2019-03-30 11:49:00 -0800

    Pass tests for RotatedShape width and height methods.

--- pyscript/rotated.py
+++ pyscript/rotated.py
@@ -9,8 +9,17 @@ class RotatedShape(Shape):
     def __init__(self, shape, rotation_angle):
         if rotation_angle not in (90, 180, 270):
             raise ValueError()
+
         self._shape = shape
         self._rotation_angle = rotation_angle
+
+        if self._rotation_angle in (90, 270):
+            self._width = self._shape._height
+            self._height = self._shape._width
+        else:
+            assert self._rotation_angle == 180
+            self._width = self._shape._width
+            self._height = self._shape._height
 
     def _get_postscript(self, center):
         shape_postscript = self._shape._get_postscript(Point(0, 0))
@@ -23,7 +32,7 @@ class RotatedShape(Shape):
         )
 
     def _get_width(self):
-        pass
+        return self._width
 
     def _get_height(self):
-        pass
+        return self._height
