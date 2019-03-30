9df59950ef25c23e990ba636b63614d65e301c6b
Jake Herrmann <jtherrmann@alaska.edu>
2019-03-30 11:07:16 -0800

    Add tests for VerticalShapes._get_width

---
+++ tests/test_vertical.py
@@ -0,0 +1,24 @@
+from pyscript import VerticalShapes, Circle, Rectangle
+
+from shape_test_case import ShapeTestCase
+
+
+class VerticalShapesTestCase(ShapeTestCase):
+
+    def test_get_width_no_shapes(self):
+        horizontal_shapes = VerticalShapes()
+        self.assertEqual(horizontal_shapes._get_width(), 0)
+
+    def test_get_width_single_shape(self):
+        horizontal_shapes = VerticalShapes(Circle(3))
+        self.assertEqual(horizontal_shapes._get_width(), 6)
+
+    def test_get_width_multiple_shapes(self):
+        horizontal_shapes = VerticalShapes(
+            Circle(1),
+            Rectangle(5, 10),
+            Circle(21),
+            Rectangle(0, 1),
+            Rectangle(3, 9)
+        )
+        self.assertEqual(horizontal_shapes._get_width(), 42)
