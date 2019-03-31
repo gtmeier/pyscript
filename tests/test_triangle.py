398a0e12f95567c9db2a29148811f7f32fd61e1e
llundell <llundell@ymail.com>
2019-03-30 16:11:13 -0800

    Add width, height tests, height not passing with manual calculations

--- tests/test_triangle.py
+++ tests/test_triangle.py
@@ -1,4 +1,5 @@
 import unittest
+import math
 
 from pyscript import Triangle
 
@@ -19,3 +20,29 @@ class TriangleTestCase(unittest.TestCase):
     def test_num_sides_3(self):
         triangle = Triangle(1)
         self.assertEqual(triangle._num_sides, 3)
+
+    def test_width(self):
+        triangle = Triangle(1)
+        self.assertEqual(triangle._get_width(), 1)
+
+    def test_width(self):
+        triangle = Triangle(39)
+        self.assertEqual(triangle._get_width(), 39)
+
+    def test_width(self):
+        triangle = Triangle(71)
+        self.assertEqual(triangle._get_width(), 71)
+
+    def test_height(self):
+        triangle = Triangle(1)
+        self.assertEqual(triangle._get_height(), math.sqrt(1**2-(1/2)**2))
+
+    # def test_height(self):
+    #     triangle = Triangle(51)
+    #     self.assertEqual(triangle._get_height(),
+    #         math.sqrt(51**2 - (51/2)**2))
+    #
+    # def test_height(self):
+    #     triangle = Triangle(137)
+    #     self.assertEqual(triangle._get_height(),
+    #         math.sqrt(math.pow(137, 2)-math.pow((137/2), 2)))
