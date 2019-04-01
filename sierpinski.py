4cb27998f4a3116c174a50eafb9f4a031a69d7bf
Jake Herrmann <jtherrmann@alaska.edu>
2019-03-31 18:06:25 -0800

    Increase sierpinski.py recursion depth.

--- sierpinski.py
+++ sierpinski.py
@@ -6,5 +6,5 @@ from pyscript import sierpinski_triangle_pages, write_postscript, Point
 
 if __name__ == "__main__":
     write_postscript(
-        sierpinski_triangle_pages(400, Point(250, 350), 7), "sierpinski.ps"
+        sierpinski_triangle_pages(400, Point(250, 350), 8), "sierpinski.ps"
     )
