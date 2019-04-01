375fdd3e3181826d11580eb1f3b6b26b0c9ed78f
Jake Herrmann <jtherrmann@alaska.edu>
2019-03-31 16:54:12 -0800

    Improve test_all_shapes.py formatting.

--- tests/test_all_shapes.py
+++ tests/test_all_shapes.py
@@ -1,8 +1,9 @@
 import unittest
 
-from pyscript import (Point, Rectangle, Spacer, Square, Circle,
-    HorizontalShapes, VerticalShapes, LayeredShapes, ScaledShape,
-    RotatedShape, Triangle, Polygon)
+from pyscript import (
+    Point, Rectangle, Spacer, Square, Circle, HorizontalShapes, VerticalShapes,
+    LayeredShapes, ScaledShape, RotatedShape, Triangle, Polygon
+)
 
 from shape_test_case import ShapeTestCase
 
@@ -15,11 +16,27 @@ class AllShapesTestCase(unittest.TestCase):
     base_circle = Circle(80)
     polygon = Polygon(5, 20)
     triangle = Triangle(20)
-    shape = HorizontalShapes(rectangle, spacer, square,
-            VerticalShapes(triangle, base_circle, LayeredShapes(ScaledShape(base_circle,
-            0.75, 0.75), polygon), LayeredShapes(ScaledShape(base_circle,
-            0.5, 0.5), RotatedShape(triangle, 180))), square,
-            spacer, rectangle)
+    vertical_shapes = VerticalShapes(
+        base_circle,
+        LayeredShapes(
+            ScaledShape(base_circle, 0.75, 0.75),
+            polygon
+        ),
+        LayeredShapes(
+            ScaledShape(base_circle, 0.5, 0.5),
+            RotatedShape(triangle, 180)
+        )
+    )
+
+    shape = HorizontalShapes(
+        rectangle,
+        spacer,
+        square,
+        vertical_shapes,
+        square,
+        spacer,
+        rectangle
+    )
 
     def test_get_width(self):
         self.assertEqual(self.shape._get_width(), 600)
